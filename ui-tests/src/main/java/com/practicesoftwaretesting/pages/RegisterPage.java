85fed76473c8d6c8124117b0066151c5fa9fba0d
Alex Shamrai <gibsonshamray@gmail.com>
2024-06-27 21:47:02 +0300

    Start writing UserTest add PageObjects + Assertions

---
+++ ui-tests/src/main/java/com/practicesoftwaretesting/pages/RegisterPage.java
@@ -0,0 +1,20 @@
+package com.practicesoftwaretesting.pages;
+
+import com.practicesoftwaretesting.asserts.RegisterPageAsserts;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selenide.$;
+
+public class RegisterPage {
+
+    public RegisterPage isLoaded() {
+        $("h3").shouldHave(text("Customer registration"));
+        return this;
+    }
+
+    public RegisterPageAsserts assertThat() {
+        return new RegisterPageAsserts();
+    }
+
+}
