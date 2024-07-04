f42943c1696860982e66903b64dbf462637290f8
Alex Shamrai <gibsonshamray@gmail.com>
2024-07-04 22:24:37 +0300

    Cleanup registered users after test

--- api-tests/src/main/java/com/practicesoftwaretesting/user/UserSteps.java
+++ api-tests/src/main/java/com/practicesoftwaretesting/user/UserSteps.java
@@ -3,17 +3,22 @@ package com.practicesoftwaretesting.user;
 import com.github.javafaker.Faker;
 import com.practicesoftwaretesting.user.model.LoginRequest;
 import com.practicesoftwaretesting.user.model.RegisterUserRequest;
+import com.practicesoftwaretesting.user.model.UserSearch;
 import com.practicesoftwaretesting.utils.ConfigReader;
 
 public class UserSteps {
 
     ConfigReader configReader = new ConfigReader();
     String defaultPassword = configReader.getProperty("default.password");
+    String adminEmail = configReader.getProperty("admin.email");
+    String adminPassword = configReader.getProperty("admin.password");
 
-    public void registerUser(String userEmail, String password) {
+    public String registerUser(String userEmail, String password) {
         var userController = new UserController();
         var registerUserRequest = buildUser(userEmail, password);
-        userController.registerUser(registerUserRequest).as();
+        return userController.registerUser(registerUserRequest)
+                .as()
+                .getId();
     }
 
     public String loginUser(String userEmail, String password) {
@@ -29,7 +34,19 @@ public class UserSteps {
         return loginUser(userEmail, defaultPassword);
     }
 
-    public RegisterUserRequest buildUser(String email, String password) {
+    public void deleteUser(String userId) {
+        var token = loginUser(adminEmail, adminPassword);
+        new UserController().withToken(token).deleteUser(userId)
+                .assertStatusCode(204);
+    }
+
+    public UserSearch searchUsers(String queryPhrase) {
+        var token = loginUser(adminEmail, adminPassword);
+        return new UserController().withToken(token).searchUsers(queryPhrase)
+                .as();
+    }
+
+    public static RegisterUserRequest buildUser(String email, String password) {
         return RegisterUserRequest.builder()
                 .firstName("John")
                 .lastName("Lennon")
@@ -45,7 +62,7 @@ public class UserSteps {
                 .build();
     }
 
-    public String getUserEmail() {
+    public static String getUserEmail() {
         return Faker.instance()
                 .friends()
                 .character()
--- api-tests/src/test/java/com/practicesoftwaretesting/CartTest.java
+++ api-tests/src/test/java/com/practicesoftwaretesting/CartTest.java
@@ -4,9 +4,11 @@ import com.practicesoftwaretesting.cart.CartController;
 import com.practicesoftwaretesting.cart.model.*;
 import com.practicesoftwaretesting.product.ProductController;
 import com.practicesoftwaretesting.product.model.ProductsRequest;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.practicesoftwaretesting.user.UserSteps.getUserEmail;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -14,13 +16,16 @@ public class CartTest extends BaseTest {
 
     private String authToken;
     private String productId;
+    private String userId;
 
     CartController cartController = new CartController();
     ProductController productController = new ProductController();
 
     @BeforeEach
     void beforeEach() {
-        authToken = registerAndLoginNewUser();
+        var email = getUserEmail();
+        userId = registerUser(email, defaultPassword);
+        authToken = loginUser(email, defaultPassword);
 
         var productRequest = ProductsRequest.builder()
                 .page(1)
@@ -52,5 +57,10 @@ public class CartTest extends BaseTest {
 
         cartController.deleteCart(cartId)
                 .assertStatusCode(204);
+    }
+
+    @AfterEach
+    void cleanup() {
+        userSteps.deleteUser(userId);
     }
 }
