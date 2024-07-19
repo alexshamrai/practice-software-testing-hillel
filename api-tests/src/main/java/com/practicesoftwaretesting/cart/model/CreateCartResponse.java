05ebe884f6b84f7e5ab98661017583ba87dcb24d
alexshamrai <alexshamrai@users.noreply.github.com>
2024-07-19 06:25:51 +0000

    deploy: ca3ca5a791541bd66786bd2ddac963a3654aba38

--- api-tests/src/main/java/com/practicesoftwaretesting/cart/model/CreateCartResponse.java
+++
@@ -1,9 +0,0 @@
-package com.practicesoftwaretesting.cart.model;
-
-import lombok.Data;
-
-@Data
-public class CreateCartResponse {
-
-    String id;
-}
--- api-tests/src/main/java/com/practicesoftwaretesting/user/model/UpdateCartResponse.java
+++
@@ -1,9 +0,0 @@
-package com.practicesoftwaretesting.user.model;
-
-import lombok.Data;
-
-@Data
-public class UpdateCartResponse {
-
-    String result;
-}
